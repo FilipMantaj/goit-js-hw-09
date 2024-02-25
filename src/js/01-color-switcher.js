c32a3630bdb54e739ae77850f6fe714f0742fd52
FilipMantaj <filip.mantaj17@o2.pl>
2024-02-25 20:26:19 +0100

    1,2 and 3 tasks changes

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+'use strict';
 
+const body = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+stopBtn.disabled = true;
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let colorSwitcher;
+
+function onStart() {
+  colorSwitcher = setInterval(switchColor, 1000);
+
+  function switchColor() {
+    return (body.style.backgroundColor = getRandomHexColor());
+  }
+
+  switchColor();
+  stopBtn.disabled = false;
+  startBtn.disabled = true;
+}
+
+function onStop() {
+  clearInterval(colorSwitcher);
+  stopBtn.disabled = true;
+  startBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', onStart);
+stopBtn.addEventListener('click', onStop);
